0d265ee10d767c8dc3d4550bdf14cbb4e4169b9c
Aditya-Maurya-101 <adityamaurya132201@gmail.com>
2026-04-30 16:28:03 +0530

    inhance project

--- Frontend/src/pages/CreatePost.jsx
+++ Frontend/src/pages/CreatePost.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const API_URL = (import.meta.env.VITE_API_URL || 'http://localhost:3000').replace(/\/+$|\s+$/g, '')
 
 const CreatePost = () => {
   const navigate = useNavigate()
--- Frontend/src/pages/Feed.jsx
+++ Frontend/src/pages/Feed.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const API_URL = (import.meta.env.VITE_API_URL || 'http://localhost:3000').replace(/\/+$|\s+$/g, '')
 
 const Feed = () => {
   const [posts, setPosts] = useState([
