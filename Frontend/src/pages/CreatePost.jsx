7c401c2bd54f08b2eef8e14ee39e43bd9c0c9db6
Aditya-Maurya-101 <adityamaurya132201@gmail.com>
2026-04-30 11:07:54 +0530

    initial commit

---
+++ Frontend/src/pages/CreatePost.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const CreatePost = () => {
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+
+    try {
+      await axios.post("https://post-web-21mt.onrender.com/create-post", formData)
+
+      navigate('/feed')
+    } catch (error) {
+      console.log(error)
+      alert("Post not created. Please try again.")
+    }
+  }
+
+  return (
+    <section className='create-post-section'>
+      <div className="create-post-card">
+        <h1>Create Post</h1>
+        <h5>Upload your image and write a caption.</h5>
+
+        <form onSubmit={handleSubmit}>
+          <label className="file-box">
+            <span>Choose Image</span>
+            <input type="file" name="image" accept="image/*" required />
+          </label>
+
+          <input
+            type="text"
+            name="caption"
+            placeholder='Enter caption'
+            required
+          />
+
+          <button type="submit">Create Post</button>
+
+          <button type="button" onClick={() => navigate('/feed')}>
+            Go to Feed
+          </button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default CreatePost
